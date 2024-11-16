9cd988251dfaae2227839791fdd0a564dd757274
Szymon P <szymon_jozef@proton.me>
2024-11-16 16:35:17 +0100

    Dodano importowalny header

--- index.php
+++ index.php
@@ -4,17 +4,10 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="./style.css" />
-    <title>Pizzeria</title>
+    <title>Pizzeria - strona główna</title>
   </head>
   <body>
-    <header>
-      <h1>Pizzeria Szczecińska</h1>
-      <nav>
-        <a href="./podstrony/zamow.php">Złóż zamówienie</a>
-        <a href="./podstrony/menu.html">Zobacz nasze menu</a>
-        <a href="./podstrony/kontakt.html">Informacje kontaktowe</a>
-      </nav>
-    </header>
+    <?php include_once __DIR__ . '/snippets/header.html' ?>
 
     <main>
       <h2>O nas:</h2>
