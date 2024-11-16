46fd42594153af1507e4d1919d6f7cc475e14a0d
Szymon P <szymon_jozef@proton.me>
2024-11-16 16:57:23 +0100

    dodano ważne phpowe rzeczy

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
+<?php
+
+define("root", __DIR__);
+
+if (isset($_SESSION['id']) || isset($_COOCKIE['login']) && isset($_COOKIE['haslo'])) {
+    header('Location: dashboard.php');
+    exit();
+}
+
+?>
 <!doctype html>
 <html lang="pl">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="./style/style.css" />
     <title>Pizzeria - strona główna</title>
-  </head>
-  <body>
+</head>
+
+<body>
+
     <?php include_once __DIR__ . '/snippets/header.html' ?>
 
     <main>
-      <h2>O nas:</h2>
-      <p>Nasza pizzeria… sraty taty</p>
+        <h2>O nas:</h2>
+        <p>Nasza pizzeria… sraty taty</p>
     </main>
 
     <footer>
-      <p>Nwm</p>
+        <p>Nwm</p>
     </footer>
-  </body>
+</body>
+
 </html>
