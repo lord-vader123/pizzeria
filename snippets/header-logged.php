37a1c3ee1ffd91a60aa9ec62e7d2c26d2e236e0a
Szymon P <szymon_jozef@proton.me>
2024-11-16 23:10:01 +0100

    Dodano obrazek w przycisku i poprawiono style

--- snippets/header-logged.php
+++ snippets/header-logged.php
@@ -1,3 +1,20 @@
+<?php
+$zdjecie_src = "";
+$stmt = $conn->prepare("SELECT zdjecie_src FROM uzytkownik WHERE login = ?");
+$stmt->bind_param("s", $_SESSION['login']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($zdjecie_src);
+
+$stmt->fetch();
+
+$pos = strpos($zdjecie_src, "/pizzeria");
+
+if ($pos !== false) {
+    $zdjecie_src = substr($zdjecie_src, $pos);
+}
+
+?>
 <header>
     <section>
         <a href="/pizzeria/index.php"><img src="/pizzeria/assets/logo.png" alt="Logo pizzeri"></a>
@@ -10,7 +27,7 @@
     </nav>
 
     <section>
-        <img src="/pizzeria/assets/user-symbol.jpg" alt="Symbol użytkownika" />
+        <img src="<?php echo $zdjecie_src ?>" alt="Symbol użytkownika" />
         <nav class="menu">
             <a class="menu-item" href="/pizzeria/php/logout.php">Wyloguj się</a>
         </nav>
