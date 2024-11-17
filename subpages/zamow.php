9ed0cb3b2efc9300bf6466d3b762827821783596
Szymon P <szymon_jozef@proton.me>
2024-11-17 14:05:54 +0100

    ukończono zamówienia

--- subpages/zamow.php
+++ subpages/zamow.php
@@ -1,4 +1,9 @@
 <?php
+// Włącz wyświetlanie błędów
+ini_set('display_errors', 1);
+
+// Ustaw poziom raportowania błędów
+error_reporting(E_ALL);
 session_start();
 define("__ROOT__", $_SESSION['__ROOT__']);
 include_once __ROOT__ . '/php/login-mysql.php';
@@ -38,6 +43,48 @@ include_once __ROOT__ . '/php/check-logged.php';
             </datalist>
             <label for="ilosc">Ile pizz pragniesz zamówić?</label>
             <input type="number" id="ilosc" name="ilosc" min="1" required>
+            <button type="reset">Zresetuj</button>
+            <button type="submit">Zamów</button>
+            <?php
+
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                $login = $_SESSION['login'];
+                $password = $_SESSION['password'];
+
+                $stmt = $conn->prepare("SELECT id FROM uzytkownik WHERE login = ? AND haslo = ?");
+                $stmt->bind_param("ss", $login, $password);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    $id = $row['id'];
+
+                    $id_pizza = $_POST['pizza'];
+                    $ilosc = $_POST['ilosc'];
+
+                    $stmt = $conn->prepare("SELECT cena FROM pizza WHERE id = ?");
+                    $stmt->bind_param("i", $id_pizza);
+                    $stmt->execute();
+                    $result = $stmt->get_result();
+                    $row = $result->fetch_assoc();
+                    $cena_pizzy = $row['cena'];
+
+                    $cena = $ilosc * $cena_pizzy;
+
+                    $stmt = $conn->prepare("INSERT INTO zamowienie (klient, pizza_id, ilosc, cena) VALUES (?, ?, ?, ?)");
+                    $stmt->bind_param("iiii", $id, $id_pizza, $ilosc, $cena);
+                    $stmt->execute();
+
+                    echo "Złożono zamówienie! <br>";
+                    echo "Cena za zamówienie: " . number_format($cena, 2) . " zł.";
+                } else {
+                    echo "Nie znaleziono użytkownika.";
+                }
+
+                $stmt->close();
+            }
+            ?>
         </form>
 
 
